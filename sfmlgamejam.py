cd133d76661e3e2a22c6114c675d40be119d33ca
Jonny <jonathan.r.paton@googlemail.com>
2016-03-02 22:02:45 +0000

    Updated time format and added hourly notice

--- sfmlgamejam.py
+++ sfmlgamejam.py
@@ -1,13 +1,32 @@
 from sopel import module, config, trigger, db
+from sopel.db import SopelDB
 import re
 import urllib
 import datetime
 
 @module.commands(r"timeleft")
-def getTimeLeft(bot, trigger):
+def timeLeft(bot, trigger):
+	timeleft = getTimeLeft()
+	hours = timeleft.seconds//3600
+	minutes = (timeleft.seconds % 3600) // 60
+	seconds = timeleft.seconds % 60
+	bot.reply(str(timeleft.days) + " days, %02d:%02d:%02d" % (hours, minutes, seconds) + " remaining!")
+
+@module.interval(10)
+def checkTimeLeft(bot):
+	timeleft = getTimeLeft()
+	hoursleft = timeleft.seconds//3600
+	db = SopelDB(bot.config)
+	lasthoursleft = db.get_nick_value(bot.nick,"hoursleft")
+	if lasthoursleft is not hoursleft :	
+		bot.say("Less than " + str(lasthoursleft) + " hours remaining!" ,"#sfmlgamejam")
+		db.set_nick_value(bot.nick,"hoursleft",hoursleft)
+
+def getTimeLeft() :
 	page = urllib.urlopen("https://sfmlgamejam.com/jams/2")
-	regexSearch = "RemainingTime = (\d+)"
-	pattern = re.compile(regexSearch)
-	results = re.findall(pattern,page.read())
+	regexpattern = "RemainingTime = (\d+)"
+	reg = re.compile(regexpattern)
+	results = re.findall(reg,page.read())
 	timeleft = datetime.timedelta(seconds = float(results[0]))
-	bot.reply(str(timeleft.days) + " days, " + str(timeleft.seconds//3600) + ":" + str((timeleft.seconds % 3600) // 60) + ":" + str(timeleft.seconds % 60))
+	return timeleft
+
